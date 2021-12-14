e5759b8ef3a8f2a198b95606444eba28a55f3c5c
James <jamesklauck@Jamess-MacBook-Air.local>
2021-12-14 17:47:44 -0500

    commit tree 98a5dc9dd61804993693bf745db0c45c5cf98085

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(array){
+   return array.slice(0,2);
+
+}
+
+
+function returnLastTwoDrivers(array){
+return array.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(fareMultiplier) {
+   return function(value) {
+      return fareMultiplier * value;
+   }
+}
+const fareDoubler = createFareMultiploier(2);
+const fareTripler = createFareMultiplier(3);
